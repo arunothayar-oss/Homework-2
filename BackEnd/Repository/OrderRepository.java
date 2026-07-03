694367285449de56f18ad88a6cd2b68a620a02b6
arunothayar-oss <arunothayar@gmail.com>
2026-07-03 19:32:38 +0530

    Removed FrontEnd and BackEnd folders

--- BackEnd/Repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.hotel.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.hotel.Entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> 
-{
-
-}
